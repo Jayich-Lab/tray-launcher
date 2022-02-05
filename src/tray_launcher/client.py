353336c2cdffb8a8bf976d00a7617e1856d6f178
Michelle Zimmermann <michellezimmermann@ucsb.edu>
2022-02-04 17:58:34 -0800

    Trying to pass a list via streams

--- src/tray_launcher/client.py
+++ src/tray_launcher/client.py
@@ -1,40 +1,90 @@
 import asyncio
 import argparse
 
-parser = argparse.ArgumentParser(prog="tray_launcher", description="Manage scripts with the tray launcher.", add_help=False)
+# async def communicate(command, args):
+#     port = 59049    #Read from a shared file, created everytime the tray_launcher is launched. Or, should we update it every say 5 hours?
 
-# parser.add_argument("-s", "--start", nargs=1, metavar="script_stem", type=str, help="Start a new script")
+#     reader, writer = await asyncio.open_connection('127.0.0.1', port)
 
-# parser.add_argument("terminate", nargs=1, metavar="script_stem", type=str, help="Terminate the script specified.")
+#     writer.write("Message".encode())   #Let's see how it's like without .encode()
 
-# parser.add_argument("restart", nargs=1, metavar="script_stem", type=str, help="Restart the script specified.")
+#     data = await reader.read()
+#     print(data.decode())
 
-# parser.add_argument("list", help="List all loaded scripts.")
+#     writer.close()
+#     #await writer.wait_closed()
 
-# parser.add_argument("load", nargs="*", metavar="script_path", type=str, help="Load some scripts.")
+async def communicate(data):
 
-# parser.add_argument("quit", help="Quit the tray launcher.")
+    reader, writer = await asyncio.open_connection('127.0.0.1', 54749)
+    print("Send: {}".format(bytes(data)))
+    writer.write(data)
 
-# parser.add_argument("logs", help="View all logs.")
+    # data = await reader.read(100)       #Need to go around this 100
+    # print("Received: {}".format(data))
 
-# parser.add_argument("log", nargs=1, metavar="script_stem", type=str, help="View log of the specified script.")     # This specified script has to be running. If parser.log=="tray_launcher", show self logs.
+    print("Close the connection")
+    writer.close()
+    # await writer.wait_closed()
 
-parser.add_argument("-h", "--help", help="Display help messages.")
-parser.add_argument("options", nargs="*", type=str)
-#Need to write the help myself
+
+
+
+parser = argparse.ArgumentParser(prog="tray_launcher", description="Manage scripts with the tray launcher.")
+
+parser.add_argument("-s", "--start", nargs="*", metavar="script_stem", type=str, help="Start new script(s).")
+
+parser.add_argument("-t", "--terminate", nargs=1, metavar="script_stem", type=str, help="Terminate the script specified.")
+
+parser.add_argument("-l", "--list", action="store_true", help="List all loaded scripts.")
+
+parser.add_argument("--load", nargs="*", metavar="script_path", type=str, help="Load some scripts.")
+
+parser.add_argument("-r", "--restart", nargs=1, metavar="script_stem", type=str, help="Restart the script specified.")
+
+parser.add_argument("--log", nargs=1, metavar="script_stem", type=str, help="View log of the specified script.")      # This specified script has to be running. If it is not running, show the same behavior as "--logs". If parser.log=="tray_launcher", show self logs.
+
+parser.add_argument("--logs", action="store_true", help="View all logs.")
+
+parser.add_argument("-f", "--front", nargs=1, metavar="script_stem", type=str, help="Bring the specified script to the foreground.")
+
+parser.add_argument("-q", "--quit", action="store_true", help="Quit the tray launcher.")
+
+
 args = parser.parse_args()
 
-print(args.multiply)
-# async def echo_client(message):
-#     reader, writer = await asyncio.open_connection('127.0.0.1', 61982)
-#     print("Send: {}".format(message))
-#     writer.write(message.encode())
+if args.start != None:
+    info = [("start").encode()]
+    for i in args.start:
+        print("Starting {}.".format(i))
+        m = i.encode()
+        print(m)
+        info.append(m)
+    print(info)
+    asyncio.run(communicate((info)))
 
-#     data = await reader.read(100)
-#     print("Received: {}".format(data.decode()))
 
-#     print("Close the connection")
-#     writer.close()
-#     # await writer.wait_closed()
-
-# asyncio.run(echo_client("Hello World"))
+elif args.terminate != None:
+    print("Terminating {}.".format(args.terminate))
+    asyncio.run(communicate("terminate", args.terminate))
+elif args.list == True:
+    print("Scripts below: ")
+    asyncio.run(communicate("list", None))
+elif args.load != None:
+    print("Loading {}.".format(args.load))
+    asyncio.run(communicate("load", args.load))
+elif args.restart != None:
+    print("Restarting {}.".format(args.restart))
+    asyncio.run(communicate("restart", args.restart))
+elif args.log != None:
+    print("Showing log of {}.".format(args.log))
+    asyncio.run(communicate("log", args.log))
+elif args.logs == True:
+    print("Opening up all logs.")
+    asyncio.run(communicate("logs", None))
+elif args.front != None:
+    print("Bringing {} to the foreground.".format(args.front))
+    asyncio.run(communicate("front", args.front))
+elif args.quit == True:
+    print("Quiting tray launcher.")
+    asyncio.run(communicate("quit", None))
--- src/tray_launcher/listener.py
+++ src/tray_launcher/listener.py
@@ -2,18 +2,34 @@ import asyncio
 
 async def handle_echo(reader, writer):
     data = await reader.read(100)
-    message = data.decode()
+    message = list(data)
+
+    info = []
+    for i in len(message):
+        info.append(message[i].decode())
     addr = writer.get_extra_info('peername')
 
     print("Received {} from {}".format(message, addr))
 
-    print("Send: {}".format(data))
-    writer.write(data)
-    await writer.drain()
+    # print("Send: {}".format(data))
+    # writer.write(data)
+    # await writer.drain()
 
     print("Close the connection")
     writer.close()
-    # await writer.wait_closed()
+
+# async def handle_echo(reader, writer):
+#     data = await reader.read()
+
+#     message = data.decode()
+#     print("Received {}".format(message))
+
+#     print("Send: {}".format(data))
+#     writer.write(data)
+#     await writer.drain()
+
+#     print("Close the connection")
+#     writer.close()
 
 async def main():
     server = await asyncio.start_server(handle_echo, '127.0.0.1', 0)
